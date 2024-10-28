cf08da41970127374345356b0ec48b9267ce1c84
FrozenDragon60Rus <naum6666@yandex.ru>
2024-10-28 17:51:59 +0300

    Add Report

---
+++ Delivery/json/Report.cs
@@ -0,0 +1,23 @@
+﻿using Delivery.sql.Table;
+using System.Text.Json;
+
+namespace Delivery.json
+{
+    internal class Report
+    {
+        string _deliveryOrder = "report.json";
+
+        public Report(IEnumerable<JoinOrder> data)
+        {
+            try
+            {
+                var jsonString = JsonSerializer.Serialize(data);
+                File.WriteAllText(_deliveryOrder, jsonString);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Во время записи отчёта произошёл сбой: {ex.Message}");
+            }
+		}
+    }
+}
